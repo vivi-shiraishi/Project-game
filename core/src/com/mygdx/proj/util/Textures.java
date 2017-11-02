d7b0e07c70bf0f808aa6764ee2392681dd5fea71
Bluthgen <33198727+Bluthgen@users.noreply.github.com>
2017-11-02 09:52:34 -0200

    Add files via upload

--- core/src/com/mygdx/proj/util/Textures.java
+++ core/src/com/mygdx/proj/util/Textures.java
@@ -38,6 +38,86 @@ public class Textures {
 	public static TextureRegion[] enemyBossRegions;
 	public static Animation<TextureRegion> enemyBossAnimation;
 
+	public static Texture bullet1RedTexture;
+	public static TextureRegion[] bullet1RedRegions;
+	public static Animation<TextureRegion> bullet1RedAnimation;
+
+	public static Texture bullet2RedTexture;
+	public static TextureRegion[] bullet2RedRegions;
+	public static Animation<TextureRegion> bullet2RedAnimation;
+
+	public static Texture bullet3RedTexture;
+	public static TextureRegion[] bullet3RedRegions;
+	public static Animation<TextureRegion> bullet3RedAnimation;
+
+	public static Texture bullet4RedTexture;
+	public static TextureRegion[] bullet4RedRegions;
+	public static Animation<TextureRegion> bullet4RedAnimation;
+
+	public static Texture bullet5RedTexture;
+	public static TextureRegion[] bullet5RedRegions;
+	public static Animation<TextureRegion> bullet5RedAnimation;
+
+	public static Texture bullet1BlueTexture;
+	public static TextureRegion[] bullet1BlueRegions;
+	public static Animation<TextureRegion> bullet1BlueAnimation;
+
+	public static Texture bullet2BlueTexture;
+	public static TextureRegion[] bullet2BlueRegions;
+	public static Animation<TextureRegion> bullet2BlueAnimation;
+
+	public static Texture bullet3BlueTexture;
+	public static TextureRegion[] bullet3BlueRegions;
+	public static Animation<TextureRegion> bullet3BlueAnimation;
+
+	public static Texture bullet4BlueTexture;
+	public static TextureRegion[] bullet4BlueRegions;
+	public static Animation<TextureRegion> bullet4BlueAnimation;
+
+	public static Texture bullet5BlueTexture;
+	public static TextureRegion[] bullet5BlueRegions;
+	public static Animation<TextureRegion> bullet5BlueAnimation;
+
+	public static Texture bullet1GreenTexture;
+	public static TextureRegion[] bullet1GreenRegions;
+	public static Animation<TextureRegion> bullet1GreenAnimation;
+
+	public static Texture bullet2GreenTexture;
+	public static TextureRegion[] bullet2GreenRegions;
+	public static Animation<TextureRegion> bullet2GreenAnimation;
+
+	public static Texture bullet3GreenTexture;
+	public static TextureRegion[] bullet3GreenRegions;
+	public static Animation<TextureRegion> bullet3GreenAnimation;
+
+	public static Texture bullet4GreenTexture;
+	public static TextureRegion[] bullet4GreenRegions;
+	public static Animation<TextureRegion> bullet4GreenAnimation;
+
+	public static Texture bullet5GreenTexture;
+	public static TextureRegion[] bullet5GreenRegions;
+	public static Animation<TextureRegion> bullet5GreenAnimation;
+
+	public static Texture bullet1PurpleTexture;
+	public static TextureRegion[] bullet1PurpleRegions;
+	public static Animation<TextureRegion> bullet1PurpleAnimation;
+
+	public static Texture bullet2PurpleTexture;
+	public static TextureRegion[] bullet2PurpleRegions;
+	public static Animation<TextureRegion> bullet2PurpleAnimation;
+
+	public static Texture bullet3PurpleTexture;
+	public static TextureRegion[] bullet3PurpleRegions;
+	public static Animation<TextureRegion> bullet3PurpleAnimation;
+
+	public static Texture bullet4PurpleTexture;
+	public static TextureRegion[] bullet4PurpleRegions;
+	public static Animation<TextureRegion> bullet4PurpleAnimation;
+
+	public static Texture bullet5PurpleTexture;
+	public static TextureRegion[] bullet5PurpleRegions;
+	public static Animation<TextureRegion> bullet5PurpleAnimation;
+
 	public static Texture obstacleTexture;
 	public static TextureRegion obstacleRegion;
 	public static Animation<TextureRegion> obstacleAnimation;
