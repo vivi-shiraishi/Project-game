1d97c59d70cd830849d11ed5128fcfed75cc1a88
Bluthgen <33198727+Bluthgen@users.noreply.github.com>
2017-11-02 09:53:53 -0200

    Add files via upload

---
+++ EnemyEntity.java
@@ -0,0 +1,29 @@
+package com.mygdx.proj.entity;
+
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.mygdx.proj.components.*;
+
+public class EnemyEntity extends Entity {
+    public EnemyEntity(float x, float y, Animation<TextureRegion> animation, float width, float height) {
+        RenderComponent render = new RenderComponent();
+        render.animation = animation;
+        this.add(render);
+
+        PositionComponent position = new PositionComponent();
+        position.x = x;
+        position.y = y;
+        this.add(position);
+
+        HitboxComponent hitbox = new HitboxComponent();
+        hitbox.x = 0;
+        hitbox.y = 0;
+        hitbox.width = width;
+        hitbox.height = height;
+        this.add(hitbox);
+
+        this.add(new DestructibleComponent());
+    }
+}
